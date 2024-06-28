39b3c0e2f8099f64eef8419123a4e029c6f0bbcc
erikam2h <89410864+erikam2h@users.noreply.github.com>
2024-06-28 16:17:13 +0200

    fixing details

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,45 +1,53 @@
-export default function Search() {
+// export default function Search() {
+//   return (
+//     <div className="p-4 flex justify-between items-center">
+//       <div className="flex">
+//         <input
+//           className="p-2 rounded-md outline-none"
+//           type="text"
+//           placeholder="Search diaries..."
+//         />
+
+//         <button
+//           type="button"
+//           className="hover:scale-110 rounded-md font-semibold bg-darkkorchid text-white w-36 h-10 font-poppins"
+//         >
+//           Search
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
+
+import React, { useState } from "react";
+
+const Search = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const handleSearch = () => {
+    console.log("Search value:", searchValue);
+  };
+
   return (
     <div className="p-4 flex justify-between items-center">
       <div className="flex">
+        {" "}
         <input
           className="p-2 rounded-md outline-none"
           type="text"
           placeholder="Search diaries..."
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
         />
-
         <button
           type="button"
-          className="hover:scale-110 rounded-md font-semibold bg-darkkorchid text-white w-36 h-10 font-poppins"
+          className="hover:scale-110 rounded-md font-semibold bg-darkorchid text-darkorchid w-36 h-10 font-poppins"
+          onClick={handleSearch}
         >
           Search
         </button>
       </div>
     </div>
   );
-}
+};
 
-// import React, { useState } from 'react';
-
-// const Search = () => { const [searchValue, setSearchValue] = useState('');
-// const handleSearch = () => { console.log('Search value:', searchValue); };
-
-// return ( <div className="p-4 flex justify-between items-center">
-//          <div className="flex"> <input className="p-2 rounded-md outline-none" type="text"
-//             placeholder="Search diaries..." value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
-
-// <button
-//       type="button"
-//       className="hover:scale-110 rounded-md font-semibold bg-darkorchid text-white w-36 h-10 font-poppins"
-//       onClick={handleSearch}
-//     >
-//       Search
-// </button>
-
-//   </div>
-// </div>
-
-// );
-// };
-
-// export default Search;
+export default Search;
